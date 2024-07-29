2af3fb9d537d5c6a0fbf30b0e6e1565e16397842
1cbyc <isaacnsisong@gmail.com>
2024-07-29 22:25:45 +0100

    i updated the main-py file with better instructions

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from data_cleaning import load_data, clean_data
 from visualization import plot_histogram, plot_scatter
 from statistical_analysis import calculate_summary_statistics, calculate_correlation
@@ -9,14 +10,23 @@ def main():
     # decided to use an absolute path for the dataset at this point
     file_path = os.path.join(os.path.dirname(__file__), '../data/dataset.csv')
 
+    if not os.path.exists(file_path):
+        print(f"file no dey here o: {file_path}")
+        sys.exit(1)
 
     # i will load and clean derived data
     data = load_data(file_path)
-    clean_data = clean_data(data)
+    # changed mind and will run an if statement for it
+    if data.empty:
+        print("data no dey to process")
+        sys.exit(1)
+
+    # clean_data = clean_data(data)
+    clean_data_df() = clean_data(data)
 
     # generate the summary stat
     # summary_stats = calculate_summary_statistics(clean_data)
-    # changing the summary stat prompt
+    # changing the summary stat instruction
     summary_stats = calculate_summary_statistics(clean_data_df)
     print("Summary Statistics:\n", summary_stats)
 
