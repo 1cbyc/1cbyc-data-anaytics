19e9324b5142b8cc9597f17475bd10588e397549
1cbyc <isaacnsisong@gmail.com>
2024-07-29 22:21:03 +0100

    i updated the data_cleaning-py script to allow error handling

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -4,6 +4,21 @@ def clean_data(df):
     df = df.dropna()
     df = df.drop_duplicates()
     return df
-
 def load_data(file_path):
-    return pd.read_csv(file_path)
+    # return pd.read_csv(file_path)
+    # updating the load_data to include error handling for empty files too
+    try:
+        df =pd.read_csv(file_path)
+        if df.empty:
+            raise ValueError("egbon your csv file is empty")
+        return df
+    except pd.errors.EmptyDataError:
+        print(f"Nno data: the file at {file_path} dey empty or no contain any columns.")
+        return pd.DataFrame()
+
+def clean_data(df):
+    df = df.dropna()
+    df = df.drop_duplicates()
+    return df
+
+
