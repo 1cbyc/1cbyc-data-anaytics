8156d97651dde38996f6e124519117d16229e673
1cbyc <isaacnsisong@gmail.com>
2024-07-29 22:32:28 +0100

    correct the data cleaning script

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-def clean_data(df):
-    df = df.dropna()
-    df = df.drop_duplicates()
-    return df
+# def clean_data(df):
+#     df = df.dropna()
+#     df = df.drop_duplicates()
+#     return df
 def load_data(file_path):
     # return pd.read_csv(file_path)
     # updating the load_data to include error handling for empty files too
